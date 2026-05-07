0f5f6b65d7659c4de551227a39e3d1bde398eb18
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-05-07 12:17:25 +0100

    feature: configure dependency injection for weather and news services

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,7 +1,11 @@
 import 'package:get_it/get_it.dart';
-import 'package:worldscope/features/countries/data/services/country_service.dart';
-import 'package:worldscope/features/countries/presentation/bloc/country_detail/country_detail_bloc.dart';
-import 'package:worldscope/features/countries/presentation/bloc/country_list/country_list_bloc.dart';
+import 'package:worldscope/data/countries/services/country_service.dart';
+import 'package:worldscope/data/news/services/news_service.dart';
+import 'package:worldscope/data/weather/services/weather_service.dart';
+import 'package:worldscope/features/deep_dive/presentation/bloc/deep_dive/deep_dive_bloc.dart';
+import 'package:worldscope/features/countries/bloc/country_detail/country_detail_bloc.dart';
+import 'package:worldscope/features/countries/bloc/country_list/country_list_bloc.dart';
+import 'package:worldscope/features/weather/bloc/weather/weather_bloc.dart';
 
 final GetIt sl = GetIt.instance;
 
@@ -21,4 +25,27 @@ Future<void> configureDependencies() async {
       countryService: sl<CountryService>(),
     ),
   );
+
+  sl.registerLazySingleton<WeatherService>(
+    WeatherService.new,
+  );
+
+  sl.registerFactory<WeatherBloc>(
+    () => WeatherBloc(
+      weatherService: sl<WeatherService>(),
+    ),
+  );
+
+  sl.registerLazySingleton<NewsService>(
+    NewsService.new,
+  );
+
+  sl.registerFactory<DeepDiveBloc>(
+    () => DeepDiveBloc(
+      countryService: sl<CountryService>(),
+      weatherService: sl<WeatherService>(),
+      newsService: sl<NewsService>(),
+    ),
+  );
+
 }
